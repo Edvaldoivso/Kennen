3edfe47bc0c2bc12a1b62f7455d16afeb6a900e6
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2020-03-19 14:32:07 -0300

    Mudanca da Função inserir pacCod e cnEvolucao
    Alterei o codigo para que ela execulte a funcao seja de forma imediata ou seja de forma apos o load da tela de 4000ms

--- Código_Botao_Salvar_Modal_Nova_Evolucao.js
+++ Código_Botao_Salvar_Modal_Nova_Evolucao.js
@@ -1,35 +1,43 @@
-var procura = $("*").hasClass("border-danger") // variavel utilizada para encontar algum campo requerido através da classe "border-danger"
+var wProcura = $("*").hasClass("border-danger"); // variavel utilizada para encontar algum campo requerido através da classe "border-danger"
+console.log(wProcura);
+if (wProcura == false){
 
-console.log(procura)
-if (procura == false) {
     // hasClass retorna true/false quando tenta encontrar algum elemento que contém a classe que foi passada
-
-    //fProp("frmshc.evolucao.main.tabs.fldImagens", "hidden", 0)
-    $("li:contains('Imagens')").addClass('cc-fld-lista-item')
-    
-    
-
-    function valor() {
-        
-        window.csPacEvo = fProp('frmshc.evolucao.main.tabs.fldEvolucao.FMEevolucao.csPacienteEvolucao', 'value')
-        console.log(csPacEvo)
-        $(document.getElementById('frmshc.evolucao.main.tabs.fldimagens.core.cspacienteevolucao')).val(csPacEvo);
-        //$(document.getElementById('frmshc.evolucao.main.tabs.fldimagens.Core.cnpaciente')).val(wPaciente);
-        fProp('frmshc.evolucao.main.tabs.fldImagens.Core.cnPaciente', 'value', wPaciente)
+    $("li:contains('Imagens')").addClass("cc-fld-lista-item");
 
 
-        console.log(wPaciente)
-        console.log(wPac)
-        
-        
-         fProp('frmshc.evolucao.main.tabs.fldEvolucao.FMEevolucao.Botoes.Salvar','hidden',0);
+    var wLoad = document.getElementsByClassName("cc-loading-fade").length
+    console.log(wLoad)
+                if (wLoad == 0){
+
+                 valor();   
+               
+    }else{
+
+        setTimeout(valor, 4000); //foi setada para a função valor ser executada 4 segundos depois de clicar no botão salvar6
+
+       
+
+        }
+       
 
 
-    }
-    setTimeout(valor, 3000); //foi setada para a função valor ser executada 4 segundos depois de clicar no botão salvar6
-    
+
+   
 } else {
 
-    console.log("é true")
-    
+    console.log("Campo requerido na tela");
+
 }
+
+
+                    function valor() {
+                        console.log("Encontrei o campo");
+                        window.csPacEvo = fProp('frmshc.evolucao.main.tabs.fldEvolucao.FMEevolucao.csPacienteEvolucao', 'value');
+                        console.log(csPacEvo);
+                        $(document.getElementById('frmshc.evolucao.main.tabs.fldimagens.core.cspacienteevolucao')).val(csPacEvo);
+                        $(document.getElementById('frmshc.evolucao.main.tabs.fldimagens.core.cnpaciente')).val(wPaciente);
+                        //fProp('frmshc.evolucao.main.tabs.fldImagens.Core.cnPaciente', 'value', wPaciente)
+                        console.log(wPaciente);
+                       fProp('frmshc.evolucao.main.tabs.fldEvolucao.FMEevolucao.Botoes.Salvar', 'hidden', 0);
+                    } //fecha funcao execulta apos terminar a carga da tela.
