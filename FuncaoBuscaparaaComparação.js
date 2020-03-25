eeaf6e0d57c78c3a22f70e786514bfe6a54f1f24
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2020-03-25 10:26:09 -0300

    Concluida a comparação
    Foi adicionado as linhas de código para comparação

--- FuncaoBuscaparaaComparação.js
+++ FuncaoBuscaparaaComparação.js
@@ -1,99 +1,96 @@
-function fComparaValor(pTabela,pCola,pColb,pCampo1,pCampo2){
-	
-	
-	//Verifica Campos Obrigatorios
-			if(pTabela == ""){
-				console.log()
-				_cc.msg("Informe a tabela",'danger')
-			}
-	
-			
-			if(pCola == "" || pColb == ""){
-				console.log("Informe as duas Colunas")
-				_cc.msg("Informe as Colunas",'danger')
-			}
-	
-		
-			
-			if(pCampo1 == "" && pCampo2 ==""){
-				
-				console.log("Informe o Campo")
-				_cc.msg("Informe um Campo",'danger')
-				
-			}
-			
-//Insere na URL de consulta se os valores estiverem OK e trata o necessário		
+function fComparaValor(pTabela, pCola, pColb, pCampo1, pCampo2) {
 
 
-			if(pTabela !==""){
-			
-				urlcompara = cc.url.ccasegd_token + "tabela="+pTabela;
-			}
+    //Verifica Campos Obrigatorios
+    if (pTabela == "") {
+        console.log()
+        _cc.msg("Informe a tabela", 'danger')
+    }
 
 
-		   if(pCampo1 !== ""){
-			
-			var wCampo1 = fProp(pCampo1,'value')
-			console.log(wCampo1 +" Campo1")
-			
+    if (pCola == "" || pColb == "") {
+        console.log("Informe as duas Colunas")
+        _cc.msg("Informe as Colunas", 'danger')
+    }
+
+
+
+    if (pCampo1 == "" && pCampo2 == "") {
+
+        console.log("Informe o Campo")
+        _cc.msg("Informe um Campo", 'danger')
+
+    }
+
+    //Insere na URL de consulta se os valores estiverem OK e trata o necessário		
+
+
+    if (pTabela !== "") {
+
+        urlcompara = cc.url.ccasegd_token + "tabela=" + pTabela;
+    }
+
+
+    if (pCampo1 !== "") {
+
+        var wCampo1 = fProp(pCampo1, 'value')
+        console.log(wCampo1 + " Campo1")
+
+    }
+
+    if (pCampo2 !== "") {
+
+        var wCampo2 = fProp(pCampo2, 'value')
+        console.log(wCampo2 + " Campo2")
+
+    }
+
+
+    if (pCola !== "") {
+        urlcompara += "&colunas=" + pCola + ",";
+    }
+
+    if (pColb !== "") {
+        urlcompara += pColb + "&where=" + pCola + "=" + '"' + wCampo1 + '"' + " & " + pColb + "=" + wCampo2 + "&limit=10";;
+    }
+
+
+    //Faz a busca no DB		
+
+    console.log(urlcompara)
+
+    $.get(urlcompara).then(function (resBusca) {
+        if (resBusca.cnRetorno == 1) {
+            console.log('RETORNO ' + resBusca.cnRetorno)
+            console.log('RETOTNO ' + resBusca.anMensagem)
+
+            _cc.msg('Erro' + resBusca.anMensagem + '', 'danger')
+            _cc.loading.hide("w-salvando-dados");
+            _cc.msg('Erro durante a operação (Ajax)', 'danger')
+
+
+        } else {
+
+            if(resBusca.data.length == 0){
+                alert('nao achou')
+            }else{
+                alert('achou')
             }
-		
-		    if(pCampo2 !== ""){
-			
-			var wCampo2 = fProp(pCampo2,'value')
-			console.log(wCampo2 +" Campo2")
-			
-		   }		
-			
-			
-			if(pCola !== ""){
-				urlcompara +="&colunas="+pCola+",";
-			}
-			
-			if(pColb !== ""){
-				urlcompara +=pColb + "&where="+ pCola + "=" + '"'+wCampo1+'"'+" & "+pColb+"="+wCampo2+"&limit=10";;
-			}
-			
-			
-//Faz a busca no DB		
-			
-			console.log(urlcompara)
-	
-            $.get(urlcompara).then(function (resBusca) {
-                if (resBusca.cnRetorno == 1) {
-                    console.log('RETORNO ' + resBusca.cnRetorno)
-                    console.log('RETOTNO ' + resBusca.anMensagem)
-                    
-                    _cc.msg('Erro' + resBusca.anMensagem + '', 'danger')
-                    _cc.loading.hide("w-salvando-dados");
-                    _cc.msg('Erro durante a operação (Ajax)', 'danger')
-					
-                    
-				}else{
-					
-					
-					var wResult = resBusca["data"][0];
-					var wValor1 = wResult[pCola]
-					var wValor2 = wResult[pColb]
-					
-			
-			
-			console.log(wValor1 +" Resultado vindo Banco")
-			console.log(wValor2 +" Resultado vindo Banco")
-			
-			if(wValor1 == pCola && wValor2 == pColb){
-				var wConfirma = true;
-				alert("Aqui")
-				
-				}
-				
-				}
-				
-			})
-			
-			
-			
-			
+            
+            
+
+
+            
+
+            
+
+        }
+
+    })
+
+
+
+
 }//Fim da Funcao
 
 
@@ -104,12 +101,10 @@ function fComparaValor(pTabela,pCola,pColb,pCampo1,pCampo2){
 
 
 /*
-
 Ir na tabela ver o que possui valor igual ao que vem dos campos combinados:
 Tem que receber as colunas que vão ser pesquisadas e os campos  que receberão os valores
 Precisa de Tabela
 Duas Colunas
 Dois campos em variaveis
-
 RETORNOS OU AÇÕES vai retornar true ou False.
 */
